765423c6c6c021a82172993d3f31ad53878380f5
Mihai Chilom <mihai.chilom@yahoo.com>
2024-06-25 20:16:18 +0300

    add a task

--- src/components/ContainerBox/ContainerBox.jsx
+++ src/components/ContainerBox/ContainerBox.jsx
@@ -1,27 +1,43 @@
+import { useState } from "react";
 import CreateTaskForm from "../CreateTaskForm/CreateTaskForm";
 import TaskCard from "../TaskCard/TaskCard";
 import "./ContainerBox.css";
 
 const ContainerBox = () => {
+  const [taskList, setTaskList] = useState([]);
+
+  const onAddTask = (task) => {
+    setTaskList([
+      ...taskList,
+      {
+        // taskName: task.name,
+        // taskDate: task.date,
+        // taskDesc: task.desc,
+        // sau spread
+        ...task,
+        taskStatus: "Todo",
+      },
+    ]);
+  };
+
+  console.log(taskList);
+
   return (
     <div className="container-box">
       <h1>Tasks</h1>
       <h4>Your tasks in your space.</h4>
       <div className="tasks">
-        <TaskCard
-          type="T-1"
-          status="todo"
-          desc="Descriere1"
-          date="24/11/2024"
-        />
-        <TaskCard
-          type="T-2"
-          status="todo"
-          desc="Descriere2"
-          date="04/12/2024"
-        />
+        {taskList.map((task, index) => (
+          <TaskCard
+            key={index}
+            name={task.name}
+            status={task.taskStatus}
+            desc={task.desc}
+            date={task.date}
+          />
+        ))}
       </div>
-      <CreateTaskForm />
+      <CreateTaskForm addTask={onAddTask} />
     </div>
   );
 };
--- src/components/CreateTaskForm/CreateTaskForm.jsx
+++ src/components/CreateTaskForm/CreateTaskForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./CreateTaskForm.css";
 import Button from "../Button/Button";
 
-const CreateTaskForm = () => {
+const CreateTaskForm = (props) => {
   const [taskName, setTaskName] = useState("");
   const [taskDate, setTaskDate] = useState("");
   const [taskdesc, setTaskDesc] = useState("");
@@ -17,9 +17,23 @@ const CreateTaskForm = () => {
     setTaskDesc(event.target.value);
   };
 
+  const newTask = {
+    name: taskName,
+    date: taskDate,
+    desc: taskdesc,
+  };
+
+  const resetState = () => {
+    setTaskName("");
+    setTaskDate("");
+    setTaskDesc("");
+  };
+
   const handleSubmitForm = (event) => {
-    console.log("Submit");
+    // console.log("Submit");
     event.preventDefault();
+    props.addTask(newTask);
+    resetState();
   };
 
   // console.log(taskName, taskDate, taskdesc);
@@ -28,16 +42,26 @@ const CreateTaskForm = () => {
       <h2>Create Task</h2>
       <form onSubmit={handleSubmitForm}>
         <label htmlFor="taskName">Task Name</label>
-        <input type="text" id="taskName" onChange={handleTaskName} />
+        <input
+          type="text"
+          id="taskName"
+          onChange={handleTaskName}
+          value={taskName}
+        />
         <label htmlFor="taskDate">Due Date</label>
         <input
           type="date"
           id="taskDate"
           placeholder="Today"
           onChange={handleTaskDate}
+          value={taskDate}
         />
         <label htmlFor="taskDescription">Task Description</label>
-        <textarea id="taskDescription" onChange={handleTaskDesc}></textarea>
+        <textarea
+          id="taskDescription"
+          onChange={handleTaskDesc}
+          value={taskdesc}
+        ></textarea>
         <Button text="Create Task" />
       </form>
     </div>
