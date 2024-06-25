9ca6e090753b3dad1abdc14cf674bc738ec0e732
Mihai Chilom <mihai.chilom@yahoo.com>
2024-06-25 20:53:32 +0300

    no tasks component created

--- src/components/ContainerBox/ContainerBox.jsx
+++ src/components/ContainerBox/ContainerBox.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CreateTaskForm from "../CreateTaskForm/CreateTaskForm";
 import TaskCard from "../TaskCard/TaskCard";
 import "./ContainerBox.css";
+import NoTasks from "../NoTasks/NoTasks";
 
 const ContainerBox = () => {
   const [taskList, setTaskList] = useState([]);
@@ -26,17 +27,22 @@ const ContainerBox = () => {
     <div className="container-box">
       <h1>Tasks</h1>
       <h4>Your tasks in your space.</h4>
-      <div className="tasks">
-        {taskList.map((task, index) => (
-          <TaskCard
-            key={index}
-            name={task.name}
-            status={task.taskStatus}
-            desc={task.desc}
-            date={task.date}
-          />
-        ))}
-      </div>
+      {/* randare conditionala */}
+      {taskList.length > 0 ? (
+        <div className="tasks">
+          {taskList.map((task, index) => (
+            <TaskCard
+              key={index}
+              name={task.name}
+              status={task.taskStatus}
+              desc={task.desc}
+              date={task.date}
+            />
+          ))}
+        </div>
+      ) : (
+        <NoTasks />
+      )}
       <CreateTaskForm addTask={onAddTask} />
     </div>
   );
